8cf03faf87954b68aded4d9c854a619f517188f9
Lurance <alphahsl@foxmail.com>
2017-09-19 20:58:14 +0800

    ...

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'index.html'
+
+
 class UcenterView(LoginRequiredMixin, TemplateView):
     template_name = 'ucenter/ucenter.html'
--- travelX/urls.py
+++ travelX/urls.py
@@ -6,6 +6,7 @@ from django.views.static import serve
 from users import views as users_views
 
 urlpatterns = [
+    url(r'^$', users_views.IndexView.as_view(), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^ucenter/$', users_views.UcenterView.as_view(), name='ucenter'),
     url(r'^media/(?P<path>.*)', serve, {"document_root": settings.MEDIA_ROOT}),
